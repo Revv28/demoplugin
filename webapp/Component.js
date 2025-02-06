d241bbbcfa973a9e5341a77516444eff45a9a64a
Revanth C M <rahulrev1234@gmail.com>
2025-02-06 09:00:21 +0000

    added fragment with dragable dialog

--- webapp/Component.js
+++ webapp/Component.js
@@ -2,8 +2,9 @@ sap.ui.define([
     "sap/ui/core/UIComponent",
     "app/demoplugin/model/models",
     "sap/ushell/Container",
-    "sap/m/Button"
-], function (UIComponent, models, Container, Button) {
+    "sap/m/Button",
+    "sap/ui/core/Fragment"
+], function (UIComponent, models, Container, Button, Fragment) {
     "use strict";
 
     return UIComponent.extend("app.demoplugin.Component", {
@@ -26,11 +27,9 @@ sap.ui.define([
                 var oFloatingButton = new Button({
                     text: "",
                     icon: "sap-icon://ai",
-                    type:sap.m.ButtonType.Emphasized,
+                    type: sap.m.ButtonType.Emphasized,
                     tooltip: "Click me",
-                    press: function () {
-                        sap.m.MessageToast.show("Floating Button Clicked!");
-                    }
+                    press: this.onOpenDialog.bind(this) // Attach dialog opening function
                 });
 
                 // Add style class
@@ -39,8 +38,29 @@ sap.ui.define([
                 // Add button to the UI
                 oRenderer.setFooter(oFloatingButton);
             }
-           
-            
+        },
+
+        onOpenDialog: function () {
+            var that = this;
+
+            if (!this._oDialog) {
+                Fragment.load({
+                    name: "app.demoplugin.view.fragments.componentContainer", // Adjust the path
+                    controller: this
+                }).then(function (oDialog) {
+                    that._oDialog = oDialog;
+                    that._oDialog.open();
+                });
+            } else {
+                this._oDialog.open();
+            }
+        },
+
+        onCloseDialog: function () {
+            if (this._oDialog) {
+                this._oDialog.close();
+            }
+        
         }
     });
 });
